ed705bd2d6ed5b166b4d86d343bf5f4fe39b9ebc
labeebarbisoft <muhammad.labeeb@arbisoft.com>
2023-08-08 15:12:55 +0500

    perforemd all revisions

--- banking.py
+++ banking.py
@@ -1,180 +1,215 @@
 import csv
-import os
 import re
 
-def is_valid_email(email):
-    return re.match(r"[^@]+@[^@]+\.[^@]+", email)
-
-def is_valid_name(name):
-    return re.match(r"^(?=.*[A-Za-z])[A-Za-z\s]+$", name)
-
-def is_valid_password(password):
-    return len(password) > 0 and password.count(' ') == 0
-
-def is_valid_amount(amount):
-    try:
-        amount = int(amount)
-        return amount > 0
-    except:
-        return False
-
-class account():
-    def __init__(self, name = "", email = "", password = "", balance = "0"):
-        self._name = name
-        self._email = email
-        self._password = password
-        self._balance = balance
-
-    @property
-    def name(self):
-        return self._name
-    @name.setter
-    def name(self, val):
-        while True:
-            self._name = input("Enter your name: ")
-            if not is_valid_name(self._name):
-                print("Invalid name format. Please try again.")
-            else:
-                break
-        
-    @property
-    def email(self):
-        return self._email
-    @email.setter
-    def email(self, val):
-        while True:
-            self._email = input("Enter your email: ")
-            if not is_valid_email(self._email):
-                print("Invalid email format. Please try again.")
-            elif self.check_duplicate_email(self._email):
-                print("An account with this email already exists. Please use a different email.")
-            else:
-                break
-        
-    @property
-    def password(self):
-        return self._password
-    @password.setter
-    def password(self, val):
-        while True:
-            self._password = input("Create a password: ")
-            if not is_valid_password(self._password):
-                print("Invalid password format. Please try again.")
-            else:
-                break
-
-    @property
-    def balance(self):
-        return self._balance
-    @balance.setter
-    def balance(self, val):
-        self._balance = val
-
-    def __str__(self):
-        return f"Name: {self._name} | Email: {self._email}"
-    
-    def get_balance(self):
-        print(f"Your account balance: ${self._balance}")
-
-    def check_duplicate_email(self, email):
-        with open("bank_accounts.csv", "r") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row[1] == email:
-                    return True
-        return False
-
-class bank():
-    def create_account(self, act):
-        with open("bank_accounts.csv", "a", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow([act.name, act.email, act.password, act.balance])
-            print("Account successfully created")
-
-    def update_account(self, act):
-        with open("bank_accounts.csv", "r") as file:
-            lines = file.readlines()
-
-        with open("bank_accounts.csv", "w") as file:
-            writer = csv.writer(file)
-            for line in lines:
-                email = list(map(str, line.split(",")))[1]
-                if email == act.email:
-                    writer.writerow([act.name, act.email, act.password, act.balance])
-                else:
-                    writer.writerow(line.strip().split(","))
-    
-    def login(self):
-        email = input("Enter your email: ")
-        password = input("Enter your password: ")
-
-        with open("bank_accounts.csv", "r") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if row[1] == email and row[2] == password:
-                    act = account(row[0], row[1], row[2], row[3])
-                    return act
-        return None
-    
-    def deposit(self, act):
-        while True:
-            amount = input("Enter the amount to deposit: ")
-            if not is_valid_amount(amount):
-                print("Invalid amount. Amount must be a number greater than 0.")
-            else:
-                act.balance = str(int(act.balance) + int(amount))
-                self.update_account(act)
-                print("Deposit successful.")
-                break
-
-    def withdraw(self, act):
-        while True:
-            amount = input("Enter the amount to withdraw: ")
-            if not is_valid_amount(amount):
-                print("Invalid amount. Amount must be a number greater than 0.")
-            elif int(amount) > int(act.balance):
-                print("Insufficient balance.")
-            else:
-                act.balance = str(int(act.balance) - int(amount))
-                self.update_account(act)
-                print("Withdrawal successful.")
-                break
+# from menu_constants import (
+#     OPEN_ACCOUNT,
+#     LOGIN,
+#     MAIN_EXIT,
+#     DEPOSIT,
+#     WITHDRAW,
+#     CHECK_BALANCE,
+#     SUB_EXIT,
+# )
 
 
-def main():
-    BANK = bank()
-    while True:
+class Menus:
+    @staticmethod
+    def display_main_menu():
         print("\n1. Open Account")
         print("2. Login")
         print("3. Exit")
-        choice = input("Enter your choice: ")
 
+    @staticmethod
+    def display_sub_menu():
+        print("\na. Deposit")
+        print("b. Withdraw")
+        print("c. Check Balance")
+        print("d. Logout")
+
+
+class Controller:
+    def is_valid_email(email):
+        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
+    def is_valid_name(name):
+        return re.match(r"^(?=.*[A-Za-z])[A-Za-z\s]+$", name)
+
+    def is_valid_password(password):
+        return len(password) > 0 and password.count(" ") == 0
+
+    @staticmethod
+    def get_valid_name():
+        while True:
+            name = input("Enter your name: ")
+            if not Controller.is_valid_name(name):
+                print("Invalid name format. Please try again.")
+            else:
+                return name
+
+    @staticmethod
+    def get_valid_email():
+        while True:
+            email = input("Enter your email: ")
+            if not Controller.is_valid_email(email):
+                print("Invalid email format. Please try again.")
+            elif DataReader.duplicate_email_exists(email):
+                print(
+                    "An account with this email already exists. Please use a different email."
+                )
+            else:
+                return email
+
+    @staticmethod
+    def get_valid_password():
+        while True:
+            password = input("Create a password: ")
+            if not Controller.is_valid_password(password):
+                print("Invalid password format. Please try again.")
+            else:
+                return password
+
+    @staticmethod
+    def get_valid_integer():
+        while True:
+            try:
+                amount = int(input("Enter amount: "))
+                if amount > 0:
+                    return amount
+                else:
+                    print("Invalid amount. Please try again.")
+            except:
+                print("Invalid amount. Please try again.")
+
+
+class User:
+    def __init__(self, name=None, email=None, password=None, balance=None):
+        if name is None:
+            self.name = Controller.get_valid_name()
+        else:
+            self.name = name
+
+        if email is None:
+            self.email = Controller.get_valid_email()
+        else:
+            self.email = email
+
+        if password is None:
+            self.password = Controller.get_valid_password()
+        else:
+            self.password = password
+
+        if balance is None:
+            self.balance = 0
+        else:
+            self.balance = balance
+
+
+class DataReader:
+    user_data = [
+        {
+            "name": "ali",
+            "email": "ali@gmail.com",
+            "password": "ali",
+            "balance": 1000,
+        },
+        {
+            "name": "asad",
+            "email": "asad@gmail.com",
+            "password": "asad",
+            "balance": 1500,
+        },
+    ]
+
+    @staticmethod
+    def duplicate_email_exists(email):
+        for user in DataReader.user_data:
+            if user["email"] == email:
+                return True
+        return False
+
+    @staticmethod
+    def get_logged_in_user(email, password):
+        for user in DataReader.user_data:
+            if user["email"] == email and user["password"] == password:
+                logged_in_user = User(user["name"], email, password, user["balance"])
+                return logged_in_user
+        return None
+
+    @staticmethod
+    def add_new_user(user):
+        DataReader.user_data.append(
+            {
+                "name": user.name,
+                "email": user.email,
+                "password": user.password,
+                "balance": user.balance,
+            }
+        )
+
+    @staticmethod
+    def update_user_balance(email, new_balance):
+        for user in DataReader.user_data:
+            if user["email"] == email:
+                user["balance"] = new_balance
+
+
+class Bank:
+    @staticmethod
+    def create_account():
+        user = User()
+        DataReader.add_new_user(user)
+
+    @staticmethod
+    def login():
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+        user = DataReader.get_logged_in_user(email, password)
+        return user
+
+    @staticmethod
+    def update_account(email, new_balance):
+        DataReader.update_user_balance(email, new_balance)
+
+    @staticmethod
+    def deposit(user):
+        amount = Controller.get_valid_integer()
+        user.balance += amount
+        Bank.update_account(user.email, user.balance)
+        print("Deposit successful.")
+
+    @staticmethod
+    def withdraw(user):
+        amount = Controller.get_valid_integer()
+        if amount > user.balance:
+            print("Not enough balance")
+        else:
+            user.balance -= amount
+            Bank.update_account(user.email, user.balance)
+            print("Withdraw successful")
+
+
+def main():
+    while True:
+        Menus.display_main_menu()
+        choice = input("Enter your choice: ")
         match choice:
             case "1":
-                ACCOUNT = account()
-                ACCOUNT.name = ""
-                ACCOUNT.email = ""
-                ACCOUNT.password = ""
-                BANK.create_account(ACCOUNT)
+                Bank.create_account()
             case "2":
-                ACCOUNT = BANK.login()
-                if ACCOUNT:
-                    print(f"Welcome, {ACCOUNT.name}!")
+                user = Bank.login()
+                if user is not None:
+                    print(f"Welcome, {user.name}!")
                     while True:
-                        print("\n1. Deposit")
-                        print("2. Withdraw")
-                        print("3. Check Balance")
-                        print("4. Logout")
+                        Menus.display_sub_menu()
                         sub_choice = input("Enter your choice: ")
-
                         match sub_choice:
-                            case "1":
-                                BANK.deposit(ACCOUNT)
-                            case "2":
-                                BANK.withdraw(ACCOUNT)
-                            case "3":
-                                ACCOUNT.get_balance()
-                            case "4":
+                            case "a":
+                                Bank.deposit(user)
+                            case "b":
+                                Bank.withdraw(user)
+                            case "c":
+                                print(f"Your balance is : ${user.balance}")
+                            case "d":
                                 print("Logging out.")
                                 break
                             case _:
@@ -186,6 +221,7 @@ def main():
                 break
             case _:
                 print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
